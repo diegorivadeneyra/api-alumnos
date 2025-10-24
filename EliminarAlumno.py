8836ddfdf09f36d8a76c105f08dfea8f80a5cd7b
diegorivadeneyra <55153225+diegorivadeneyra@users.noreply.github.com>
2025-10-23 22:20:00 -0500

    Create EliminarAlumno.py

---
+++ EliminarAlumno.py
@@ -0,0 +1,23 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Entrada (json)
+    tenant_id = event['body']['tenant_id']
+    alumno_id = event['body']['alumno_id']
+
+    # Proceso
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('t_alumnos')
+    response = table.delete_item(
+        Key={
+            'tenant_id': tenant_id,
+            'alumno_id': alumno_id
+        }
+    )
+
+    # Salida (json)
+    return {
+        'statusCode': 200,
+        'mensaje': 'Alumno eliminado correctamente',
+        'response': response
+    }
